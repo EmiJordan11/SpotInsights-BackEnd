2eb6626aa87c65eeede8082dafb8b43cd90c7c5a
EmiJordan11 <emiliano.e.jordan@gmail.com>
2025-02-28 18:25:33 -0300

    init: Initial project configuration

---
+++ src/main/java/com/emijordan/Spotinsights/SpotinsightsApplication.java
@@ -0,0 +1,13 @@
+package com.emijordan.Spotinsights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotinsightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotinsightsApplication.class, args);
+	}
+
+}
