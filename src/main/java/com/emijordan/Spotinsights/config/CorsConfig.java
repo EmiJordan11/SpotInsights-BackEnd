a8d1f60f812a6a9a492c4ae241bbec4174f6c4f6
EmiJordan11 <emiliano.e.jordan@gmail.com>
2025-05-15 11:18:46 -0300

    fix: configure CORS from specific origin

---
+++ src/main/java/com/emijordan/Spotinsights/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.emijordan.Spotinsights.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Value("${frontend.origin}")
+    private String frontendOrigin;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(frontendOrigin)
+                        .allowedMethods("POST");
+            }
+        };
+    }
+}
